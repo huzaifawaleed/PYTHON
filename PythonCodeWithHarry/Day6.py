79085646f64adb1d0e0793e1e005c050179b6088
Huzaifa Waleed <huzaifainaam225@gmail.com>
2025-07-06 04:48:13 +0500

    1

---
+++ PythonCodeWithHarry/Day6.py
@@ -0,0 +1,21 @@
+#Variables
+
+a = 10
+b = "huzaifa"
+c = 10.1
+d = False
+
+# Data Types
+print("The type of", type(a))
+print("The type of", type(b))
+print("The type of", type(c))
+print("The type of", type(d))
+
+#LIST
+list = [8, 10.23, [20,40],"Huzaifa", "Waleed"]
+print(list)
+
+#TUPLE
+tuple = (("Green","Red"),("Yellow","Blue"))
+print(tuple)
+
