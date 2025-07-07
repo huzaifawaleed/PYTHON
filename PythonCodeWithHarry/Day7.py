4dddc9f2bae60afd91ca83dd8234e2382b52b1cf
Huzaifa Waleed <huzaifainaam225@gmail.com>
2025-07-07 06:21:47 +0500

    python

---
+++ PythonCodeWithHarry/Day7.py
@@ -0,0 +1,27 @@
+# Calculator
+
+# a = int(input("Enter a Number: "))
+# b = int(input("Enter a Number: "))
+
+a = 20
+b = 10
+ans = a + b
+print("The sum of ",a," and ",b," is:", ans)
+
+ans1 = a - b
+print("The subtration of ",a," and ",b," is:", ans1)
+
+ans2 = a * b
+print("The multiplication of ",a," and ",b," is:", ans2)
+
+ans3 = a / b
+print("The divison of ",a," and ",b," is:", ans3)
+
+ans4 = a ** b
+print("The exponential of ",a," and ",b," is:", ans4)
+
+ans5 = a % b
+print("The modulo of ",a," and ",b," is:", ans5)
+
+ans6 = a // b
+print("The double division of ",a," and ",b," is:", ans6)
