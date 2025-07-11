23d472f7a64ab788e1ed20b04c9905218a39a56d
Huzaifa Waleed <huzaifainaam225@gmail.com>
2025-07-11 15:47:55 +0500

    py

---
+++ PythonCodeWithHarry/Day.17.py
@@ -0,0 +1,11 @@
+# FOR LOOPS.
+
+
+# Write a for loop to print numbers from 1 to 10, each on a new line.
+
+# numbers = "huzaifa"
+# for n in numbers:
+#     print(n)
+#     if n == "z":
+#         print("this is Z")
+      
