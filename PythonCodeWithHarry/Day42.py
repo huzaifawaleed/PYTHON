b0e2eec39d824929d7863cf407c51a14fa4bd17a
Huzaifa Waleed <huzaifainaam225@gmail.com>
2025-08-03 01:28:41 +0500

    py

--- PythonCodeWithHarry/Day42.py
+++ PythonCodeWithHarry/Day42.py
@@ -2,4 +2,9 @@
 
 marks = [30,50,87,90,87,89,98]
 for idx, mark in enumerate(marks,start=1):
-    print(idx,mark)
+    print(f"{idx}:{mark}")
+
+
+name = "Huzaifa"
+for id, c in enumerate(name,start=1):
+    print(f"{id}: {c} ")
