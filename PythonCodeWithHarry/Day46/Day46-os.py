8f1c5831f27fb8d631123c17b45b6c588686abf6
Huzaifa Waleed <huzaifainaam225@gmail.com>
2025-08-08 02:10:15 +0500

    py

---
+++ PythonCodeWithHarry/Day46/Day46-os.py
@@ -0,0 +1,18 @@
+# | Function / Variable    | Description                                               |
+# | ---------------------- | --------------------------------------------------------- |
+# | `os.getcwd()`          | Get current working directory 📍                          |
+# | `os.chdir(path)`       | Change the current directory 🔄                           |
+# | `os.listdir(path)`     | List files and folders in a directory 📃                  |
+# | `os.mkdir(path)`       | Make a new directory 📂                                   |
+# | `os.makedirs(path)`    | Make nested directories (like `mkdir -p`) 🪜              |
+# | `os.remove(file)`      | Delete a file ❌                                           |
+# | `os.rmdir(dir)`        | Remove an empty directory 🚮                              |
+# | `os.rename(src, dst)`  | Rename/move a file or directory 🔁                        |
+# | `os.path.exists(path)` | Check if path exists ✅                                    |
+# | `os.path.isfile(path)` | Check if it's a file 🗃️                                  |
+# | `os.path.isdir(path)`  | Check if it's a directory 🗂️                             |
+# | `os.path.join()`       | Join paths (cross-platform safe) 🪢                       |
+# | `os.environ`           | Access environment variables 🌍                           |
+# | `os.system(cmd)`       | Run system commands (like in terminal) ⚙️                 |
+# | `os.name`              | Get OS type: `'posix'` (Linux/mac) or `'nt'` (Windows) 🧠 |
+# | `os.stat(file)`        | Get metadata (size, modified date, etc.) 📊               |
