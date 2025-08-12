b501c6968b694f67f65e78112bbdf8065dd8cc56
Huzaifa Waleed <huzaifainaam225@gmail.com>
2025-08-13 03:44:35 +0500

    py

--- PythonCodeWithHarry/Day44.py
+++ PythonCodeWithHarry/Day44.py
@@ -1,47 +1,47 @@
 # How Import Works.
 
 
-# import math
+import math
 
-# res = math.sqrt(10)
-# print(res)
+res = math.sqrt(10)
+print(res)
 
 # importing a specific functions or variables from a module using the 'from' keyword.
 
-# from math import pi,sqrt
+from math import pi,sqrt
 
-# a = 10
-# res = pi*a
-# print(res)
+a = 10
+res = pi*a
+print(res)
 
-# res = sqrt(15) 
-# print(res)
-# res1 = res * pi
-# print(res1)
+res = sqrt(15) 
+print(res)
+res1 = res * pi
+print(res1)
 
 # Python also allows you to rename imported modules using the 'as' keyword.
 
-# import math as zafi
+import math as zafi
 
-# res = zafi.sqrt(100)
-# print(res)
-# res2 = res * zafi.pi
-# print(res2)
+res = zafi.sqrt(100)
+print(res)
+res2 = res * zafi.pi
+print(res2)
 
 
 # dir() is used to view the all variables and functions that are available in module.
 
-# import math
+import math
 
-# print(dir(math))
+print(dir(math))
 
-# import csv
-# print(dir(csv))
+import csv
+print(dir(csv))
 
-# import huzaifa 
-# huzaifa.welcome()
+import huzaifaday45 
+huzaifaday45.welcome()
 
 
-# import huzaifa as hu
-# hu.welcome()
-# print(hu.hello)
+import huzaifaday45 as hu
+hu.welcome()
+print(hu.hello)
