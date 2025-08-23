0266e181bd9f991b219b7d7cf18155740df50c29
Huzaifa Waleed <huzaifainaam225@gmail.com>
2025-08-24 02:47:53 +0500

    py

--- PythonCodeWithHarry/Day54.py
+++ PythonCodeWithHarry/Day54.py
@@ -1,1 +1,18 @@
-# is and == in python..
+# is and == in python.
+
+# Lists are mutable the value can be change. 
+a = [1,2,3]
+b = [1,2,3]
+print(a is b)  # FALSE
+print(a == b)  # TRUE
+
+# strings and integers are immutable the value cannot be change.
+c = 12
+d = 12
+print(c is d) # TRUE
+print(c == d) # TRUE
+
+e = "Huzaifa"
+f = "Huzaifa"
+print(e is f) # TRUE
+print(e == f) # TRUE
