1db0c246cbe5c5d083daac58cc49ca4e48d0ba0f
Huzaifa Waleed <huzaifainaam225@gmail.com>
2025-08-28 01:50:13 +0500

    py

---
+++ PythonCodeWithHarry/Day56.py
@@ -0,0 +1,10 @@
+# Introduction of OOPS.
+from word2number import w2n
+
+# Taking input from user
+text_num = input("Enter a number in words: ")
+
+# Converting to integer
+num = w2n.word_to_num(text_num)
+
+print("Converted integer:", num)
